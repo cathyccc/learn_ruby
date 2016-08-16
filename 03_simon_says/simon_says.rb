4874cdab8cb587dfd7d750d45c92ff0669e502a5
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-16 16:20:45 -0400

    added method taking just the first word and titleize

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -14,3 +14,24 @@ end
 def start_of_word(word,num_letters)
   word[0..num_letters-1]
 end
+
+def first_word(message)
+  message_each_word = message.split
+  return message_each_word[0]
+end
+
+def titleize(message)
+  words_array = message.split
+
+  words_array.each do |word|
+    if word == "of" || word == "the" || word == "over" || word == "and"
+      word.downcase
+    else
+      word.capitalize!
+    end
+  end
+
+  words_array[0].capitalize!
+
+  words_array.join(" ")
+end
