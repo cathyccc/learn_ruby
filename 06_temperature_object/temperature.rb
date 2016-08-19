7263e44a9e2a5d7d8ddf872b0081f6e81c812aab
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-19 09:48:28 -0400

    added cases for converting to fahrenhei and celcius for each method

--- 06_temperature_object/temperature.rb
+++ 06_temperature_object/temperature.rb
@@ -4,17 +4,29 @@ class Temperature
   end
 
   def to_fahrenheit
-    @hash[:f]
+    if @hash.has_key?(:f)
+      fahrenheit = @hash[:f]
+    else
+      fahrenheit = @hash[:c] * 9 / 5.0 + 32
+    end
+    return fahrenheit
   end
 
-  def to_celcius
-    
+  def to_celsius
+    if @hash.has_key?[:c]
+      celsius = @hash[:c]
+    else
+      celsius = (@hash[:f] - 32) * 5 / 9.0
+    end
+    return celsius
   end
-
 end
 
 class Fahrenheit < Temperature
 end
 
 class Celsius < Temperature
+  def initialize
+
+  end
 end
