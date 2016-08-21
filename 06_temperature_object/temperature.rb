01ef50a0ca42e279423d9f93cb4f042803868af2
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-21 18:26:56 -0400

    modified to_celsius method

--- 06_temperature_object/temperature.rb
+++ 06_temperature_object/temperature.rb
@@ -13,20 +13,29 @@ class Temperature
   end
 
   def to_celsius
-    if @hash.has_key?[:c]
+    if @hash.has_key?(:c)
       celsius = @hash[:c]
     else
       celsius = (@hash[:f] - 32) * 5 / 9.0
     end
     return celsius
   end
+
+  # assert_equal 50, Temperature.in_celsius(50).to_celsius
+  # assert_equal 122, Temperature.in_celsius(50).to_fahrenheit
+
+  def self.in_celsius(temperature)
+    temperature
+  end
+
+  def self.in_fahrenheit(temperature)
+    temperature
+  end
+
 end
 
 class Fahrenheit < Temperature
 end
 
 class Celsius < Temperature
-  def initialize
-
-  end
 end
